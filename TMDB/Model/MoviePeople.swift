9b4d2a1c578c7cfdb5b3e7fd39c1f0d1b3a245ea
LEESANGNAM <fgh0116@naver.com>
2023-08-14 01:25:31 +0900

    상세화면 테이블뷰 크기 축소 & 모델 필요없는주석 삭제

--- TMDB/Model/MoviePeople.swift
+++ TMDB/Model/MoviePeople.swift
@@ -7,42 +7,6 @@
 
 import Foundation
 
-
-//
-//"cast": [
-//    {
-//      "adult": false,
-//      "gender": 2,
-//      "id": 587506,
-//      "known_for_department": "Acting",     //
-//      "name": "Shameik Moore",
-//      "original_name": "Shameik Moore",
-//      "popularity": 43.036,
-//      "profile_path": "/uJNaSTsfBOvtFWsPP23zNthknsB.jpg",
-//      "cast_id": 705,
-//      "character": "Miles Morales / Spider-Man (voice)",
-//      "credit_id": "6489a4f8e375c000e251ab48",
-//      "order": 0
-//    },
-//
-//
-//    "crew": [
-//        {
-//          "adult": false,
-//          "gender": 2,
-//          "id": 7624,
-//          "known_for_department": "Writing",
-//          "name": "Stan Lee",
-//          "original_name": "Stan Lee",
-//          "popularity": 24.195,
-//          "profile_path": "/kKeyWoFtTqOPsbmwylNHmuB3En9.jpg",
-//          "credit_id": "611752420f0da5005f4e35ee",
-//          "department": "Writing",
-//          "job": "Characters"
-//        },
-// 공통 필요한거 name known_for_department: Acting이 두개 포함 제외 시키고, profile_path, 타입? cast, crew
-//cast : character   crew job
-
 enum CreditType: String,CaseIterable{
     case cast,crew
     
